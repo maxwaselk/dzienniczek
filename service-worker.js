b1bb162903c7be9d1fb981ddf25bc6f3de0a9b49
maxwaselk <69905880+maxwaselk@users.noreply.github.com>
2024-10-12 20:25:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
 const CACHE_NAME = 'dzienniczek-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    '/assets/icons/icon-192x192.png',
-    '/assets/icons/icon-512x512.png'
+    '/dzienniczek/',
+    '/dzienniczek/index.html',
+    '/dzienniczek/style.css',
+    '/dzienniczek/app.js',
+    '/dzienniczek/manifest.json',
+    '/dzienniczek/assets/icons/icon-192x192.png',
+    '/dzienniczek/assets/icons/icon-512x512.png'
 ];
 
 // Instalacja Service Workera
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function (cache) {
+            .then(function(cache) {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-// Aktywacja Service Workera i usunięcie starych cache'ów
-self.addEventListener('activate', function (event) {
+// Aktywacja Service Workera i usunięcie starych cache
+self.addEventListener('activate', function(event) {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then(function (cacheNames) {
+        caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.map(function (cacheName) {
+                cacheNames.map(function(cacheName) {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
                     }
@@ -36,14 +36,11 @@ self.addEventListener('activate', function (event) {
 });
 
 // Fetching z cache'u
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+            .then(function(response) {
+                return response || fetch(event.request);
             })
     );
 });
