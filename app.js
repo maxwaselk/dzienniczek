7bab1c1445c89f37d5b215c6349d72618d7dedcd
maxwaselk <69905880+maxwaselk@users.noreply.github.com>
2024-10-12 20:24:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -93,13 +93,10 @@ document.addEventListener('DOMContentLoaded', function () {
         wyswietlSpotkania();
     });
 
-    // Wyświetlenie spotkań przy załadowaniu
-    wyswietlSpotkania();
-
-    // Rejestracja Service Workera
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => console.log('Service Worker zarejestrowany', registration))
-            .catch(error => console.log('Błąd rejestracji Service Workera:', error));
-    }
+// Rejestracja Service Workera z właściwą ścieżką
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/dzienniczek/service-worker.js')
+        .then(registration => console.log('Service Worker zarejestrowany', registration))
+        .catch(error => console.log('Błąd rejestracji Service Workera:', error));
+}
 });
